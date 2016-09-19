b94bc8f1290735e9d2bc5fd808bc2bd05b01b7d0
Jordan Terrell <jterrell@wans.net>
2016-09-19 08:04:52 -0500

    Created script to install Terraform...

---
+++ scripts/install-terraform.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./common.sh
+
+if ! which terraform; then
+    installing "Terraform"
+
+    mkdir -p /tmp/terraform
+
+    pushd /tmp/terraform >> /dev/null
+    curl -sL https://releases.hashicorp.com/terraform/0.7.3/terraform_0.7.3_linux_amd64.zip -o terraform.zip
+
+    unzip terraform.zip
+    install terraform /usr/local/bin/terraform
+
+    popd >> /dev/null
+    rm -rf /tmp/terraform
+fi
