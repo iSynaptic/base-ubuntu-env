48c55b2466651ae25e650b98f7aac45c3e9e1a9d
Jordan Terrell <jterrell@wans.net>
2016-09-20 14:26:42 -0500

    Tweaked RVM install script...

--- scripts/install-rvm.sh
+++ scripts/install-rvm.sh
@@ -6,5 +6,5 @@ if ! which rvm >> /dev/null; then
     installing "RMV"
 
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    curl -sSL https://get.rvm.io | bash -s stable --ruby
+    sudo curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
 fi
