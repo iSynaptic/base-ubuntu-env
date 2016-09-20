400a0bccc5fb9d0b1981beed63074847b9453258
Jordan Terrell <jterrell@wans.net>
2016-09-20 14:01:46 -0500

    Git tool install is now based on a minimum version...

--- scripts/install-git.sh
+++ scripts/install-git.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 source ./common.sh
+(
 
-if (! which git >> /dev/null) || [ "$(git --version)" == "git version 2.5.0" ]; then
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+
+MINIMUM_GIT_VERSION=2.9.3
+EXISTING_GIT_VERSION=$(git --version | awk '{print $3}' || 0.0.0)
+
+if (! which git >> /dev/null) || version_gt $MINIMUM_GIT_VERSION $EXISTING_GIT_VERSION ; then
     installing "Git"
 
     add-apt-repository ppa:git-core/ppa
     apt-get update
     apt-get install -y git
 fi
+
+)
