4cbffccd7bb685e53adb587a0356a4bd6e452d96
Jordan Terrell <jterrell@wans.net>
2016-09-22 07:59:56 -0500

    Updated RVM installer to include bundler...

--- scripts/install-rvm.sh
+++ scripts/install-rvm.sh
@@ -7,4 +7,5 @@ if ! which rvm >> /dev/null; then
 
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     sudo curl -sSL https://get.rvm.io | sudo bash -s stable --ruby
+    sudo gem install bundler
 fi
