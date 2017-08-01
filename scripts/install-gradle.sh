c9ad23b837fcaf6c728c8c1d56cc4a8cccc2f30a
Jordan Terrell <jterrell@wans.net>
2017-08-01 10:58:24 -0500

    Updating README to reflect recent theme change and adding Gradle install script...

---
+++ scripts/install-gradle.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ./common.sh
+
+if [ ! -f /opt/gradle/gradle-4.0/bin/gradle ]; then
+    installing "Gradle"
+
+    curl -o /tmp/gradle.zip -sSL https://services.gradle.org/distributions/gradle-4.0-bin.zip
+    mkdir -p /opt/gradle
+
+    unzip -d /opt/gradle /tmp/gradle.zip
+    rm /tmp/gradle.zip
+fi
+
+if ! which gradle > /dev/null; then
+    doing "Linking to Gradle install"
+    ln -s /opt/gradle/gradle-4.0/bin/gradle /usr/local/bin/gradle
+fi
