3b2c9d8cdcf8a18d6b239951f1172ba65ff16e54
Jordan Terrell <jterrell@wans.net>
2017-08-15 10:35:13 -0500

    Added installation scripts for Wrk and Vegeta...

---
+++ scripts/install-wrk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./common.sh
+
+if ! which wrk > /dev/null; then
+    installing "Wrk 4.0.2 (may take a while)"
+    
+    cat > /tmp/make-wrk.sh <<EOF
+#!/bin/bash
+
+apt-get update
+apt-get install -y curl build-essential libssl-dev git
+
+cd /tmp
+git clone https://github.com/wg/wrk.git /tmp/wrk-git
+cd /tmp/wrk-git
+make
+mv /tmp/wrk-git/wrk /tmp/wrk
+cd /tmp
+rm -rf /tmp/wrk-git /tmp/wrk.tar.gz
+EOF
+
+    chmod +x /tmp/make-wrk.sh
+    docker run --rm -v /tmp:/tmp debian:9.1 /tmp/make-wrk.sh
+    install /tmp/wrk /usr/local/bin/wrk
+    rm /tmp/wrk /tmp/make-wrk.sh
+fi
