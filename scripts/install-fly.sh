d2c9f44504e72a30444aeb7b3b36b40468a5515f
Jordan Terrell <jterrell@wans.net>
2017-11-02 12:31:24 -0500

    Upgraded Fly to 3.6.0, Node to 8.x, and added Google Cloud SDK install script...

--- scripts/install-fly.sh
+++ scripts/install-fly.sh
@@ -3,8 +3,8 @@
 source ./common.sh
 
 if ! which fly >> /dev/null; then
-    installing "Concourse Fly 3.4.1"
-    curl -o /tmp/fly -sSL https://github.com/concourse/concourse/releases/download/v3.4.1/fly_linux_amd64
+    installing "Concourse Fly 3.6.0"
+    curl -o /tmp/fly -sSL https://github.com/concourse/concourse/releases/download/v3.6.0/fly_linux_amd64
 
     install /tmp/fly /usr/local/bin/fly
 
