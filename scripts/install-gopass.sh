3966158c0277b34a451592d690bed7cf3bd84e9c
Jordan Terrell <jterrell@wans.net>
2017-12-18 16:04:38 -0600

    Upgraded Gopass installation script...

--- scripts/install-gopass.sh
+++ scripts/install-gopass.sh
@@ -5,7 +5,7 @@ source ./common.sh
 if ! which gopass >> /dev/null; then
     installing "Gopass"
 
-    curl -sL https://www.justwatch.com/gopass/releases/1.1.0/gopass_1.1.0_amd64.deb -o /tmp/gopass.deb
+    curl -sSL https://github.com/justwatchcom/gopass/releases/download/v1.6.4/gopass-1.6.4-linux-amd64.deb -o /tmp/gopass.deb
     dpkg -i /tmp/gopass.deb
 
     rm /tmp/gopass.deb
