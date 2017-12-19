c465d582a4bdf2e8c7c5c5b9bf84a4732aface21
Jordan Terrell <jterrell@wans.net>
2017-12-19 10:57:29 -0600

    Adding warning/alerting functions to common shell script...

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,6 +4,11 @@ if [ "$COMMON_SCRIPT_LOADED" = "1" ]; then
     return
 fi
 
+export ANSI_COLOR_RED='\033[0;31m'
+export ANSI_COLOR_GREEN='\033[0;32m'
+export ANSI_COLOR_YELLOW='\033[0;33m'
+export ANSI_COLOR_NONE='\033[0m' # No Color
+
 function installing() {
     doing "Installing" "$1"
 }
@@ -13,12 +18,39 @@ function configuring() {
 }
 
 function doing() {
-GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
-
     DOING=$1
     MSG=$2
-    printf "${GREEN}$DOING ${MSG}...${NC}\n"
+
+    println_with_color "$ANSI_COLOR_GREEN" "$DOING $MSG..."
+}
+
+function warn() {
+    println_with_color "$ANSI_COLOR_YELLOW" "$1"
+}
+
+function alert() {
+    println_with_color "$ANSI_COLOR_YELLOW" "$1"
+}
+
+function with_color() {
+    COLOR=$1
+    MSG=$2
+
+    echo "$COLOR$MSG$ANSI_COLOR_NONE"
+}
+
+function print_with_color() {
+    COLOR=$1
+    MSG=$2
+
+    printf "$(with_color "$COLOR" "$MSG")"
+}
+
+function println_with_color() {
+    COLOR=$1
+    MSG=$2
+
+    printf "$(with_color "$COLOR" "$MSG")\n"
 }
 
 function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
