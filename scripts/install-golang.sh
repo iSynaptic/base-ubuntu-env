914dc6601d86468cf014ed4f869fb11a9c49dbed
Jordan Terrell <jterrell@wans.net>
2018-01-18 15:23:31 -0600

    Adding 'dep' tool to Golang install script...

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -3,7 +3,11 @@
 source ./common.sh
 
 if [ ! -f /usr/local/go/bin/go ]; then
-    installing "Go Language"
+    installing "Go Language (and tools)"
     curl -o /tmp/go.tar.gz -sSL https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz
     tar -C /usr/local -xzf /tmp/go.tar.gz
+
+    curl -o /tmp/dep -sSL https://github.com/golang/dep/releases/download/v0.3.2/dep-linux-amd64
+    install /tmp/dep /usr/local/bin/dep
+    rm /tmp/dep
 fi
