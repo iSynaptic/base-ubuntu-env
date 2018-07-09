bf643ecf39f9d79539a41d0538451e80e61c1bc4
Jordan Terrell <jterrell@wans.net>
2018-07-09 13:58:42 -0500

    Fixing typo in .NET install script...

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -10,5 +10,5 @@ if ! which dotnet > /dev/null; then
     sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 
     apt-get update
-    apt-get install dotnet-sdk-2.1.4
+    apt-get install -y dotnet-sdk-2.1.4
 fi
