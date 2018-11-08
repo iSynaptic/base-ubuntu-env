daad979f6af7aac3d49900aa5d77839756643706
Jordan Terrell <jterrell@wans.net>
2018-11-08 16:41:30 -0600

    Adding JVM Async Profiler to Java install script...

--- scripts/install-oracle-java8.sh
+++ scripts/install-oracle-java8.sh
@@ -11,3 +11,17 @@ if ! which javac > /dev/null; then
     apt-get update
     apt-get install -y oracle-java8-installer
 fi
+
+if [ ! -d /etc/jvm-async-profiler ]; then
+    installing "JVM Async Profiler"
+
+    curl -o /tmp/ap.tar.gz -sSL https://github.com/jvm-profiling-tools/async-profiler/releases/download/v1.4/async-profiler-1.4-linux-x64.tar.gz
+    mkdir -p /tmp/ap
+    (
+        cd /tmp/ap
+        tar -vxzf /tmp/ap.tar.gz
+    )
+
+    mv /tmp/ap /etc/jvm-async-profiler
+    rm /tmp/ap.tar.gz
+fi
