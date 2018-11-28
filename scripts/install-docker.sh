5ce40d35160240491a9462f5fe33230fa7391433
Jordan Terrell <jterrell@wans.net>
2018-11-28 15:55:13 -0600

    Adding dive to docker install script...

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -187,3 +187,10 @@ if ! which reg > /dev/null; then
     sudo install /tmp/reg /usr/local/bin/reg
     rm /tmp/reg
 fi
+
+if ! which dive > /dev/null; then
+    installing "dive"
+    curl -o /tmp/dive.deb -sSL https://github.com/wagoodman/dive/releases/download/v0.3.0/dive_0.3.0_linux_amd64.deb
+    dpkg -i /tmp/dive.deb
+    rm /tmp/dive.deb
+fi
